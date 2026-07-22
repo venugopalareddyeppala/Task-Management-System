3d04cd305d810d247c377f30f38e1b6fb84bb261
Venu Gopala Reddy Eppala <venugopalareddy46@gmail.com>
2026-07-22 16:11:23 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,31 +2,21 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary py-2 shadow-sm">
       <div className="container">
-
         <Link className="navbar-brand fw-bold" to="/">
           Task Management System
         </Link>
 
         <div className="ms-auto">
-
-          <Link
-            to="/"
-            className="btn btn-light me-2"
-          >
+          <Link to="/" className="btn btn-light btn-sm me-2">
             Home
           </Link>
 
-          <Link
-            to="/add"
-            className="btn btn-warning"
-          >
+          <Link to="/add" className="btn btn-warning btn-sm">
             Add Task
           </Link>
-
         </div>
-
       </div>
     </nav>
   );
